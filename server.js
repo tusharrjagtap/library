77a90dd7bd76d1acbd401f7ec98054e6390e3572
Tushar Rajabhau Jagtap <99384364+tusharrjagtap@users.noreply.github.com>
2022-02-20 15:55:01 +0530

    fromT
    123

--- server.js
+++ server.js
@@ -12,16 +12,11 @@ app.use(bodyparser.urlencoded({ extended: false }));
 
 ///define routes
 app.get("/", function(req, res) {
-    res.sendFile("public/empdata.html", { root: __dirname });
+    res.sendFile("public/index.html", { root: __dirname });
 
 })
 
-app.post("/submit_data", function(req, res) {
-    var empid = req.body.empid;
-    console.log(empid + "  " + req.body.ename + "  " + req.body.sal)
-    mongoop.savedata(req.body);
-    res.send(empid + "submitted successfully");
-})
+
 
 app.listen(3000);
 console.log("server is running on port 3000");
